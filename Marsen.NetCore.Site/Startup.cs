6f826d0258072f38b043653e3b776fd0100b8dae
marklin <marklin@nine-yi.com>
2019-02-17 18:54:49 +0800

    from website to mvc

--- Marsen.NetCore.Site/Startup.cs
+++ Marsen.NetCore.Site/Startup.cs
@@ -45,7 +45,7 @@ namespace Marsen.NetCore.Site
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -54,7 +54,12 @@ namespace Marsen.NetCore.Site
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
