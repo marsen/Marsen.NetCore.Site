6d08c007885e3ee619a1de5c50f69d3d122201ab
marklin <marklin@nine-yi.com>
2019-04-26 01:18:06 +0800

    方案架構重構

---
+++ src/00.Tests/Marsen.Test.Integrated/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Marsen.Test.Integrated
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CheckAPI()
+        {
+
+        }
+    }
+}
---
+++ src/99.Tools/Marsen.CodeGen/Marsen.CodeGen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Marsen.CodeGen
+{
+    class Program
+    {
+        private const string SolutionPath = @"D:\Repo\Marsen\Marsen.NetCore.Site\src\";
+        private const string DaProject = @"Marsen.NetCore.DA";
+
+        static void Main(string[] args)
+        {
+            var entityName = "Shop";
+            Console.WriteLine($"SolutionPath: {SolutionPath}");
+            Console.WriteLine($"Entity: {entityName}");
+            Console.WriteLine("=== Start Processing ===");
+        }
+    }
+}
