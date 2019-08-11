fc733ac5331c624259c530f75e616a2904d81bdd
marklin <marklin@nine-yi.com>
2019-08-11 22:28:51 +0800

    Product - After Run Code Gen

---
+++ src/02.Logic/Marsen.Business.Logic/Entities/ProductEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Marsen.Business.Logic.Entities
+{    
+    /// <summary>
+    /// ProductEntity
+    /// </summary>
+    public class ProductEntity
+    {        		
+		/// <summary>
+		/// Id
+		/// </summary>
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Name
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Information
+		/// </summary>
+		public string Information { get; set; }
+
+		/// <summary>
+		/// Spec
+		/// </summary>
+		public string Spec { get; set; }
+
+		/// <summary>
+		/// Picture
+		/// </summary>
+		public string Picture { get; set; }
+
+
+    }
+}
