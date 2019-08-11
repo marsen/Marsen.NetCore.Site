89664476a62e346f9d801be55291c88d8e0e6feb
marklin <marklin@nine-yi.com>
2019-08-12 01:18:08 +0800

    run code gen

---
+++ src/02.Logic/Marsen.Business.Logic/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using Marsen.Business.Logic.Entities;
+using Marsen.Business.Logic.Interface;
+
+namespace Marsen.Business.Logic.Services
+{
+    public partial class ProductService 
+    {
+        /// <summary>
+        /// The Product Service
+        /// </summary>
+        private readonly IProductStorage _productStorage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductService" /> class.
+        /// </summary>
+        public ProductService(IProductStorage productStorage)
+        {
+            this._productStorage = productStorage;
+        }
+
+        /// <summary>
+        /// Reads the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>ProductEntity</returns>
+        public ProductEntity Read(long id)
+        {
+            return this._productStorage.Read(id);
+        }
+    }
+}
