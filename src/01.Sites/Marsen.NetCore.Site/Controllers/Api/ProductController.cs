681c07c1c1606a22492efe9b3328154cdceb6f71
marklin <marklin@nine-yi.com>
2019-08-14 00:24:02 +0800

    加入 Product api

---
+++ src/01.Sites/Marsen.NetCore.Site/Controllers/Api/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marsen.NetCore.Site.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult("Get");
+        }
+
+        [HttpPost]
+        public JsonResult Post()
+        {
+            return new JsonResult("Post");
+        }
+
+        
+        [HttpPut]
+        public JsonResult Put()
+        {
+            return new JsonResult("Put");
+        }
+        
+        [HttpDelete]
+        public JsonResult Delete()
+        {
+            return new JsonResult("Delete");
+        }
+    }
+}
