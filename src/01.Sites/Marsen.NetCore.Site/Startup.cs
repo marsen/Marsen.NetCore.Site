477c38d947500029df0859df089bb066923c2bc5
Marsen <thisismysoul@gmail.com>
2020-06-08 15:46:52 +0800

    add Use Rounting

--- src/01.Sites/Marsen.NetCore.Site/Startup.cs
+++ src/01.Sites/Marsen.NetCore.Site/Startup.cs
@@ -54,11 +54,15 @@ namespace Marsen.NetCore.Site
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+            //app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                //endpoints.MapRazorPages();
             });
         }
     }
