929b94715b55dd984a14a11e11b13d708d59f11a
Marsen <thisismysoul@gmail.com>
2020-06-09 08:55:12 +0800

    update automapper

--- src/03.Data/Marsen.NetCore.DA/Storage/ProductStorage.cs
+++ src/03.Data/Marsen.NetCore.DA/Storage/ProductStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using AutoMapper;
 using Marsen.Business.Logic.Entities;
 using Marsen.Business.Logic.Services;
 using Marsen.NetCore.DA.Models;
@@ -8,12 +9,18 @@ namespace Marsen.NetCore.DA.Storage
 {
     public class ProductStorage:IRead<ProductEntity>
     {
+        private readonly IMapper _mapper;
+
+        public ProductStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public ProductEntity Read(long id)
         {
             using (var context=new MARSContext())
             {
                 var result = context.Product.FirstOrDefault(x => x.ProductId == id);
-                return AutoMapper.Mapper.Map<ProductEntity>(result);
+                return _mapper.Map<ProductEntity>(result);
             }
         }
     }
--- src/03.Data/Marsen.NetCore.DA/Storage/ShopStorage.cs
+++ src/03.Data/Marsen.NetCore.DA/Storage/ShopStorage.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Linq;
+﻿using AutoMapper;
 using Marsen.Business.Logic.Entities;
 using Marsen.Business.Logic.Services;
 using Marsen.NetCore.DA.Models;
+using System.Linq;
 
 namespace Marsen.NetCore.DA.Storage
 {
-    public class ShopStorage:IRead<ShopEntity>
+    public class ShopStorage : IRead<ShopEntity>
     {
+        private readonly IMapper _mapper;
+
+        public ShopStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         public ShopEntity Read(long id)
         {
-            using (var context=new PhobosContext())
+            using (var context = new PhobosContext())
             {
                 var result = context.Shop.FirstOrDefault(x => x.ShopId == id);
-                return AutoMapper.Mapper.Map<ShopEntity>(result);
+                return _mapper.Map<ShopEntity>(result);
             }
         }
     }
