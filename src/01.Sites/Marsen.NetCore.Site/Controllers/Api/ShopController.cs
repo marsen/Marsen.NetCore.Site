66f4fa426c0d6a15d79b4afd13261223241099fe
Marsen <thisismysoul@gmail.com>
2020-06-10 09:00:51 +0800

    add shop controller

---
+++ src/01.Sites/Marsen.NetCore.Site/Controllers/Api/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using Marsen.Business.Logic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marsen.NetCore.Site.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(long id)
+        {
+            ShopService shopService = new ShopService();
+            var result = shopService.Get(id);
+            return Ok(result);
+        }
+    }
+}
--- src/02.Logic/Marsen.Business.Logic/Services/ShopService.cs
+++ src/02.Logic/Marsen.Business.Logic/Services/ShopService.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Marsen.Business.Logic.Entities;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Marsen.Business.Logic.Services
 {
     public class ShopService
     {
+        public ShopEntity Get(long id)
+        {
+            //// TODO: Get Data From DAO
+            var shopList = new List<ShopEntity>
+            {
+                new ShopEntity {Id = 1, IsEnable = true, Title = "Marsen Shop"},
+                new ShopEntity {Id = 2, IsEnable = false, Title = "Marsen Shop Close"},
+                new ShopEntity {Id = 3, IsEnable = true, Title = "Kobe Shop"},
+                new ShopEntity {Id = 4, IsEnable = true, Title = "Tom's Shop"},
+            };
+            return shopList.First(s => s.Id == id);
+        }
     }
 }
