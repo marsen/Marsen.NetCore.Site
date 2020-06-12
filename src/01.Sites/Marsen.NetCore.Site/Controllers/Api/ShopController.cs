388be15c81d978dee019ad23601559b8c87c42c6
Marsen <thisismysoul@gmail.com>
2020-06-12 11:21:18 +0800

    controller add logger

--- src/01.Sites/Marsen.NetCore.Site/Controllers/Api/ShopController.cs
+++ src/01.Sites/Marsen.NetCore.Site/Controllers/Api/ShopController.cs
@@ -1,5 +1,8 @@
-﻿using Marsen.Business.Logic.Services;
+﻿using System;
+using Marsen.Business.Logic.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Marsen.NetCore.Site.Controllers.Api
 {
@@ -8,15 +11,18 @@ namespace Marsen.NetCore.Site.Controllers.Api
     public class ShopController : ControllerBase
     {
         private readonly IShopService _shopService;
+        private readonly ILogger<ShopController> _logger;
 
-        public ShopController(IShopService shopService)
+        public ShopController(IServiceProvider serviceProvider)
         {
-            _shopService = shopService;
+            _shopService = serviceProvider.GetRequiredService<IShopService>();
+            _logger = serviceProvider.GetRequiredService<ILogger<ShopController>>();
         }
 
         [HttpGet("{id}")]
         public ActionResult<string> Get(long id)
         {
+            _logger.LogTrace(id.ToString());
             var result = _shopService.Get(id);
             return Ok(result);
         }
